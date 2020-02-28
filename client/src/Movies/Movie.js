bf7f101053fcc9262fc26287ac580185d6b46550
Ike Otu <ikeotu@msn.com>
2020-02-28 00:13:45 -0800

    Movie.js completed, receiving props

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import MovieCard from './MovieCard';
-import UpdateMovie from './UpdateMovieForm';
+
 
 function Movie(props) {
-  console.log('PROPS DATA', props)
+  
   const [movie, setMovie] = useState(null);
-  const {id} = useParams();
+  const {id} = useParams();//useParams() allows for use of a dynamic id when querying the server.
 
   const fetchMovie = id => {
     axios
@@ -16,8 +16,22 @@ function Movie(props) {
       .catch(err => console.log(err.response));
   };
 
-  const saveMovie = () => {
+  const saveMovie = () => {//invokes addToSavedList() which is passed via props from App.js, then passes it movie array at line 9.
     props.addToSavedList(movie);
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+    props.history.push(`/update-movie/${movie.id}`)
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        props.setMovie(res.data);
+        props.history.push('/');
+      })
   };
 
   useEffect(() => {
@@ -35,8 +49,16 @@ function Movie(props) {
       <div className='save-button' onClick={saveMovie}>
         Save
       </div>
+      
+      <div className='update-movie-button' onClick={handleUpdate}>
+        Edit Movies
+      </div>
+
+      <div className='delete-button' onClick={handleDelete}>
+        Remove movie from list
+      </div>
     </div>
   );
-}
+};
 
 export default Movie;
